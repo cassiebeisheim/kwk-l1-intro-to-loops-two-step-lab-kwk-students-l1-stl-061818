3d9459770b497206dd701c2e9d76b3050dd45400
cassiebeisheim <cassiebeisheim@gmail.com>
2018-06-19 20:23:49 +0000

    automatically backed up by learn

--- counter_loops.rb
+++ counter_loops.rb
@@ -1,8 +1,25 @@
 def cha_cha_slide
   moves_completed=0 
 loop do
-    if moves_completed == 10
+    if moves_completed == 5
       break
     end
+    puts "slide to the left"
+    sleep (0.1)
+    puts "slide to the right"
+    sleep(0.1)
+    puts "take it back now yall"
+    sleep(0.1)
+    puts "one hop this time"
+    sleep(1)
+    puts "one hop this time"
+    sleep(1)
+    puts "criss cross"
+    sleep(2)
+    puts "criss cross"
+    sleep(2)
+    puts "everybody clap your hands"
+    sleep(5)
+  end
     
 end
